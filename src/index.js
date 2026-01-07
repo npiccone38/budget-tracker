df21a3ee20a86db1c1892341b81cf7679e86cf4b
Nick Piccone <nicpic13@outlook.com>
2026-01-06 23:57:14 -0500

    Progress on the new entry form
    It doesn't work all the way yet but I need to go to sleep.
    I'm kinda getting the hang of it.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import "./style.css";
+
+// payment object factory
+function createPayment(amount, date, location){
+    return {amount, date, location};
+}
+
+const atl = document.querySelector("#atl");
+atl.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const fd = new FormData(atl);
+    const hist_container = document.querySelector(".history-container");
+
+    const entry = document.createElement("div");
+    entry.classList.add("entry-container");
+    entry.textContent = "Amount: " + fd.get(amount) + " Date: " + fd.date + " Location: " + fd.location;
+    hist_container.prepend(entry);
+
+});
+
