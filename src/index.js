7bee2cfe5825518b52ddd205e19cdc387080b27d
Nick Piccone <nicpic13@outlook.com>
2026-01-08 13:57:54 -0500

    The entry log updates correctly
    The add form works and the entry log now shows the
    results in a presentable way. I do want to add
    edit and delete buttons in the future.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 import "./style.css";
 
-// payment object factory - maybe not needed
-// function createPayment(amount, date, location){
-//     return {amount, date, location};
-//}
-
-
-
 const atl = document.querySelector("#atl");
 atl.addEventListener("submit", (e) => {
     e.preventDefault();
     const fd = new FormData(atl);
-    const hist_container = document.querySelector(".history-container");
+    const history_container = document.querySelector(".history-container");
 
     const entry = document.createElement("div");
     entry.classList.add("entry-container");
-    entry.textContent = "Amount: " + fd.get("amount") + " Date: " + fd.get("date") + " Location: " + fd.get("location");
-    hist_container.prepend(entry);
+
+    const entry_location = document.createElement("div");
+    entry_location.classList.add("entry-location");
+    entry_location.textContent = fd.get("location");
+
+    const entry_amount = document.createElement("div");
+    entry_amount.classList.add("entry-amount");
+    entry_amount.textContent = "$" + fd.get("amount");
+
+    const entry_date = document.createElement("div");
+    entry_date.classList.add("entry-date");
+    entry_date.textContent = fd.get("date");
+
+    history_container.prepend(entry);
+
+    entry.appendChild(entry_location);
+    entry.appendChild(entry_amount);
+    entry.appendChild(entry_date);
 
     document.getElementById("atl").reset();
 });
